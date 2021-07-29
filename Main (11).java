cfd937f5b983ccf6cfbbdbe256ef4dc9dd7175db
aathika <83359584+Aathika08@users.noreply.github.com>
2021-07-29 22:10:27 +0530

    Add files via upload

---
+++ Main (11).java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+class Main{  
+ static int n1=0,n2=1,n3=0;    
+ static void printFibonacci(int count){    
+    if(count>0){    
+         n3 = n1 + n2;    
+         n1 = n2;    
+         n2 = n3;    
+         System.out.print(" "+n3);   
+         printFibonacci(count-1);    
+     }    
+ }    
+ public static void main(String args[]){    
+  int count=10;    
+  System.out.print(n1+" "+n2);   
+  printFibonacci(count-2);
+ }  
+}
